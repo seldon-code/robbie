ea43bcada6e76db6a25c5301c75ec1e1957d0648
Moritz Sallermann <moritzsallermann@gmail.com>
2023-10-22 19:06:13 +0000

    test_fc_layer: backward propagation

--- test/test_fc_layer.cpp
+++ test/test_fc_layer.cpp
@@ -7,12 +7,13 @@
 
 TEST_CASE( "Test_FullyConnectedLayer" )
 {
-    long input_size  = 2;
-    long output_size = 3;
-    auto layer       = Robbie::FCLayer<double>( input_size, output_size );
-    auto y_get       = Robbie::Vector<double>( output_size );
-    auto y_actual    = Robbie::Vector<double>( output_size );
-    auto X           = Robbie::Vector<double>( input_size );
+    long input_size      = 2;
+    long output_size     = 3;
+    double learning_rate = 0.01;
+    auto layer           = Robbie::FCLayer<double>( input_size, output_size );
+    auto y_get           = Robbie::Vector<double>( output_size );
+    auto y_actual        = Robbie::Vector<double>( output_size );
+    auto X               = Robbie::Vector<double>( input_size );
     X << -3, 2;
 
     auto weights = Robbie::Matrix<double>( input_size, output_size );
@@ -21,4 +22,33 @@ TEST_CASE( "Test_FullyConnectedLayer" )
     y_get = layer.forward_propagation( X );
 
     REQUIRE( y_get.rows() == output_size ); // output Vector should be of size output_size
+
+    // Let's say the loss function is the sum of outputs E = y1 + ... + yN
+    // Then dE/dY = (1, ..., 1)^T
+    auto output_error = Robbie::Vector<double>::Ones( output_size );
+
+    // The input error should be dE/dX
+    auto input_error = layer.backward_propagation( output_error, learning_rate );
+
+    // Get the y0 for some input
+    auto x0    = Robbie::Vector<double>::Zero( input_size );
+    auto loss0 = layer.forward_propagation( x0 ).sum();
+
+    // changing any component of x0 to x0[i] -> x0[i] + epsilon, should change the sum of outputs
+    // by input_error[i] * epsilon we will test this individually for each component
+    double epsilon = 1e-2; // Can basically choose any epsilon here since the function is quadratic anyways
+    for( int i = 0; i < x0.size(); i++ )
+    {
+        Robbie::Vector<double> x_new = x0;
+        x_new[i] += epsilon;
+        Robbie::Vector<double> y_new = layer.forward_propagation( x_new );
+        auto loss_new                = y_new.sum();
+        auto derivative_fd           = ( loss_new - loss0 ) / epsilon;
+        REQUIRE_THAT( derivative_fd, Catch::Matchers::WithinAbs( input_error[i], 1e-6 ) );
+
+        fmt::print( "loss0 = {}\n", loss0 );
+        fmt::print( "loss_new = {}\n", loss_new );
+        fmt::print( "input_error[{}] = {}\n", i, input_error[i] );
+        fmt::print( "derivative_fd = {}\n", derivative_fd );
+    }
 }
