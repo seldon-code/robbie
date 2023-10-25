41383937835064f0ab228f1c4de0c2e3af087bba
Moritz Sallermann <moritzsallermann@gmail.com>
2023-10-25 11:46:16 +0000

    Changed ReLU implementation. Needes some static_cast to work with non-double types

--- include/activation_functions.hpp
+++ include/activation_functions.hpp
@@ -26,14 +26,18 @@ public:
     template<typename Derived>
     static auto f( const Eigen::MatrixBase<Derived> & x )
     {
-        auto greater_than_zero = []( typename Derived::Scalar x ) { return std::max( x, 0.0 ); };
+        // The static_cast is necessary to make different scalar types than double compile
+        using scalar           = typename Derived::Scalar;
+        auto greater_than_zero = []( scalar x ) { return std::max<scalar>( x, static_cast<scalar>( 0.0 ) ); };
         return x.array().unaryExpr( greater_than_zero );
     }
 
     template<typename Derived>
     static auto df( const Eigen::MatrixBase<Derived> & x )
     {
-        auto one_if_greater_than_zero = []( typename Derived::Scalar x ) { return x >= 0.0 ? 1.0 : 0.0; };
+        // The static_cast is necessary to make different scalar types than double compile
+        using scalar                  = typename Derived::Scalar;
+        auto one_if_greater_than_zero = []( scalar x ) { return static_cast<scalar>( x >= 0.0 ? 1.0 : 0.0 ); };
         return x.array().unaryExpr( one_if_greater_than_zero );
     }
 };
